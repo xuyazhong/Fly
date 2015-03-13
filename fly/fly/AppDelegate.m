bb499dbd2e53c95cb45c4a1be1e95debf8558f2f
xuyazhong <xuyazhong@foxmail.com>
2015-03-13 10:22:03 +0800

    add push

--- fly/fly/AppDelegate.m
+++ fly/fly/AppDelegate.m
@@ -46,7 +46,6 @@
     /*
     [MAMapServices sharedServices].apiKey = @"fbc09ee09efe913d00eebc86568d1232";
     */
-    NSLog(@"path:%@",NSHomeDirectory());
     [MAMapServices sharedServices].apiKey = @"fbc09ee09efe913d00eebc86568d1232";
     //[MAMapServices sharedServices].apiKey = @"fbc09ee09efe913d00eebc86568d1232";
     [WeiboSDK enableDebugMode:YES];
@@ -67,7 +66,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        //[[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
         [APService registerForRemoteNotificationTypes:(UIUserNotificationTypeBadge |UIUserNotificationTypeSound |UIUserNotificationTypeAlert)categories:nil];
         [[UIApplication sharedApplication] registerForRemoteNotifications];
 
@@ -168,11 +167,14 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+    [application setApplicationIconBadgeNumber:0];
+    [application cancelAllLocalNotifications];
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
